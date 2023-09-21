715cdc841bd25f7268242e02e8bdcdce2196b3e4
Aadya Srivastava <aadya28.srivastava@gmail.com>
2023-09-21 14:39:21 +0530

    Implemented SQL data retrieval and board display on the Workspace. #10

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask, render_template, request, redirect, url_for
+from flask import jsonify
 
 app = Flask(__name__, template_folder='templates')
 
